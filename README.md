dcf2ecbb9e55d0e41ef035817a3e8f974eefe47c
minoic <minoic2020@gmail.com>
2022-10-11 22:41:43 +0800

    add catalog script file

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 
 问题皆来源于下面的两个链接：
 
- 1. <https://stackoverflow.com/questions/tagged/c%2b%2b?tab=Votes>
- 2. <https://stackoverflow.com/questions/tagged/c?tab=Votes>
+1. <https://stackoverflow.com/questions/tagged/c%2b%2b?tab=Votes>
+2. <https://stackoverflow.com/questions/tagged/c?tab=Votes>
 
 > 注：001 - 117 基本符合 votes 排序（从高到低）；从 118 开始，只挑选有意义的问题翻译，不再遵循 votes 排序高低依次翻译。
+
+## 目录
+
+<!-- catalog -->
+
+<!-- catalog -->
---
+++ scripts/catalog.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	readme, err := os.OpenFile("README.md", os.O_RDWR, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	readmeB, err := io.ReadAll(readme)
+	if err != nil {
+		panic(err)
+	}
+	readmeS := string(readmeB)
+	flag := "<!-- catalog -->"
+	splits := strings.Split(readmeS, flag)
+	ctl := strings.Builder{}
+	ctl.WriteString("\n\n")
+	err = filepath.Walk("question", func(path string, info fs.FileInfo, err error) error {
+		if info.IsDir() || !strings.Contains(info.Name(), ".md") {
+			return nil
+		}
+		ctl.WriteString(fmt.Sprintf("[%s](%s)\n\n", strings.Trim(info.Name(), "[]"), strings.ReplaceAll(path, " ", "%20")))
+		return nil
+	})
+	readmeS = splits[0] + flag + ctl.String() + flag + splits[2]
+	if err != nil {
+		panic(err)
+	}
+	err = readme.Truncate(0)
+	if err != nil {
+		panic(err)
+	}
+	_, err = readme.Seek(0, 0)
+	if err != nil {
+		panic(err)
+	}
+	_, err = readme.WriteString(readmeS)
+	if err != nil {
+		panic(err)
+	}
+}
